d18909dd52c7f15f93bb774a983583bc8480868d
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2022-12-21 15:52:37 +0530

    Add files via upload

--- src/components/data.js
+++ src/components/data.js
@@ -1,16 +1,14 @@
 export const USER_DATA = [
     {
       id: 1,
-      firstName: "Suman",
-      lastName: "Kumar",
-      email: "suman@test.com",
-      age: 10
+      name: "Testuser1",
+      email: "user1@test.com",
+      phone: 9876543213
     },
     {
       id: 2,
-      firstName: "Rahul",
-      lastName: "Kumar",
-      email: "rahul@test.com",
-      age: 15
+      name: "Testuser2",
+      email: "user2@test.com",
+      phone: 9765432456
     },
   ];
