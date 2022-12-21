549e2510a571c45f1d2ec8cc16e2629d526ec321
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2022-12-21 13:37:01 +0530

    Update Givepet.js

--- src/components/Givepet.js
+++ src/components/Givepet.js
@@ -89,7 +89,7 @@ const Givepet = () => {
       var userData = tx.objectStore("userData");
       const users = userData.getAll();
 
-      const dataIdIndex = userData.index("users");
+//       const dataIdIndex = userData.index("users");
 
       users.onsuccess = (query) => {
         setAllUsers(query.srcElement.result);
